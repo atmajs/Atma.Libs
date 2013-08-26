b6903c830d6bf9ba588d22d0e6f270b08d277e7f
Alexander Kit <source.kit@gmail.com>
2013-08-26 16:04:31 +0200

    build:0.8.50

--- build/process.js
+++ build/process.js
@@ -1,32 +1,45 @@
 include
 
-.js('process/Sources.js')
-.load('libs.json')
+	.js(
+		'process/Sources.js',
+		'publish.js'
+	)
+	.load('libs.json')
 
-.done(function(resp){
-	
-	var libs = resp.load.libs,
-		Sources = resp.Sources,
-		Uglify = resp.Uglify;
-	
-	
-	if (typeof libs === 'string') 
-		libs = JSON.parse(libs);
+	.done(function(resp){
 		
-	
-	include
-		.routes(libs.routes)
-		.load(libs.libraries)
-		.done(function(resp){
+		var libs = resp.load.libs,
+			Sources = resp.Sources,
+			Uglify = resp.Uglify,
+			publish = resp.publish;
+		
+		
+		if (typeof libs === 'string') 
+			libs = JSON.parse(libs);
 			
-			var pckg = Sources.prepair(libs.pckg, resp.load);
-			
-			new Sources(pckg)
-				.process()
-				.save();
-			
-		})
-	
-	
+		
+		include
+			.routes(libs.routes)
+			.load(libs.libraries)
+			.done(function(resp){
+				
+				var pckg = Sources.prepair(libs.pckg, resp.load);
+				
+				new Sources(pckg)
+					.process()
+					.save();
+				
 
-});
+				if (app.config.cli.params.publish){
+					publish(function(error){
+						if (error) 
+							logger.error(error);
+							
+						logger.log('Done'.green.bold);
+					});
+				}
+			})
+		
+		
+
+	});
