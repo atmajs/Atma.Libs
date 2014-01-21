7e45dc3d10c711a8b0d9adbcbbce2890ef97d198
Alexander Kit <source.kit@gmail.com>
2014-01-21 17:03:58 +0100

    build:0.8.86

--- browser/index-dev.js
+++ browser/index-dev.js
@@ -3,7 +3,7 @@
 	
 	// source /src/license.txt
 /*!
- * ClassJS v1.0.44
+ * ClassJS v1.0.45
  * Part of the Atma.js Project
  * http://atmajs.com/
  *
@@ -21,18 +21,11 @@
 			? global
 			: window
 			,
-		_isCommonJS = false
 		_exports
 		;
 
     
-	if (typeof exports !== 'undefined' && (root == null || root === exports || root === _global)){
-		// raw commonjs module
-		_isCommonJS = true;
-        root = exports;
-    }
-	
-    _exports = root || _global;
+	_exports = root || _global;
     
 
     function construct(){
@@ -48,9 +41,8 @@
 	// Browser OR Node
     construct();
 	
-	if (_isCommonJS) 
+	if (typeof module !== 'undefined') 
 		module.exports = _exports.Class;
-	
 	
 }(this, function(global, exports){
 	"use strict";
@@ -1356,120 +1348,126 @@
 	// source /src/business/Deferred.js
 	function Deferred(){}
 	
-	Deferred.prototype = {
-		_isAsync: true,
-			
-		_done: null,
-		_fail: null,
-		_always: null,
-		_resolved: null,
-		_rejected: null,
+	(function(){
 		
-		defer: function(){
-			this._rejected = null;
-			this._resolved = null;
-		},
-		
-		resolve: function() {
-			this._fail = null;
-			this._resolved = arguments;
-	
-			var _done = this._done,
-				_always = this._always,
+		Deferred.prototype = {
+			_isAsync: true,
 				
-				imax, i;
+			_done: null,
+			_fail: null,
+			_always: null,
+			_resolved: null,
+			_rejected: null,
 			
-			this._done = null;
-			this._always = null;
+			defer: function(){
+				this._rejected = null;
+				this._resolved = null;
+			},
 			
-			if (_done != null) {
-				imax = _done.length;
-				i = -1;
-				while ( ++i < imax ) {
-					fn_apply(_done[i], this, arguments);
-				}
-				_done.length = 0;
+			resolve: function() {
+				var done = this._done,
+					always = this._always
+					;
+				
+				this._resolved = arguments;
+				
+				dfr_clearListeners(this);
+				arr_callOnce(done, this, arguments);
+				arr_callOnce(always, this, [ this ]);
+				
+				return this;
+			},
+			
+			reject: function() {
+				var fail = this._fail,
+					always = this._always
+					;
+				
+				this._rejected = arguments;
+				
+				dfr_clearListeners(this);
+				arr_callOnce(fail, this, arguments);
+				arr_callOnce(always, this, [ this ]);
+		
+				return this;
+			},
+			
+			resolveDelegate: function(){
+				return fn_proxy(this.resolve, this);
+			},
+			
+			rejectDelegate: function(){
+				return fn_proxy(this.reject, this);
+			},
+			
+			done: function(callback) {
+				
+				return dfr_bind(
+					this,
+					this._resolved,
+					this._done || (this._done = []),
+					callback
+				);
+			},
+			
+			fail: function(callback) {
+				
+				return dfr_bind(
+					this,
+					this._rejected,
+					this._fail || (this._fail = []),
+					callback
+				);
+			},
+			
+			always: function(callback) {
+				
+				return dfr_bind(
+					this,
+					this._rejected || this._resolved,
+					this._always || (this._always = []),
+					callback
+				);
+			},
+		};
+	
+		// PRIVATE
+		
+		function dfr_bind(dfr, arguments_, listeners, callback){
+			if (callback == null) 
+				return dfr;
+			
+			if ( arguments_ != null) 
+				fn_apply(callback, dfr, arguments_);
+			else 
+				listeners.push(callback);
+			
+			return dfr;
+		}
+		
+		function dfr_clearListeners(dfr) {
+			dfr._done = null;
+			dfr._fail = null;
+			dfr._always = null;
+		}
+		
+		function arr_callOnce(arr, ctx, args) {
+			if (arr == null) 
+				return;
+			
+			var imax = arr.length,
+				i = -1,
+				fn;
+			while ( ++i < imax ) {
+				fn = arr[i];
+				
+				if (fn) 
+					fn_apply(fn, ctx, args);
 			}
-	
-			if (_always != null) {
-				imax = _always.length;
-				i = -1;
-				while ( ++i < imax ) {
-					_always[i].call(this, this);
-				}
-			}
-	
-			return this;
-		},
+			arr.length = 0;
+		}
 		
-		reject: function() {
-			this._done = null;
-			this._rejected = arguments;
-			
-			var _fail = this._fail,
-				_always = this._always,
-				imax, i;
-			
-			this._fail = null;
-			this._always = null;
-	
-			if (_fail != null) {
-				imax = _fail.length;
-				i = -1;
-				while ( ++i < imax ) {
-					fn_apply(_fail[i], this, arguments);
-				}
-			}
-	
-			if (_always != null) {
-				imax = _always.length;
-				i = -1;
-				while ( ++i < imax ) {
-					_always[i].call(this, this);
-				}
-			}
-	
-			return this;
-		},
-		
-		resolveDelegate: function(){
-			return fn_proxy(this.resolve, this);
-		},
-		
-		rejectDelegate: function(){
-			return fn_proxy(this.reject, this);
-		},
-		
-		done: function(callback) {
-			if (this._resolved != null)
-				fn_apply(callback, this, this._resolved);
-			else
-				(this._done || (this._done = [])).push(callback);
-	
-			return this;
-		},
-		
-		fail: function(callback) {
-			
-			if (this._rejected != null)
-				fn_apply(callback, this, this._rejected);
-			else
-				(this._fail || (this._fail = [])).push(callback);
-	
-			return this;
-		},
-		
-		always: function(callback) {
-		
-			if (this._rejected != null || this._resolved != null)
-				callback.call(this, this);
-			else
-				(this._always || (this._always = [])).push(callback);
-	
-			return this;
-		},
-	};
-	
+	}());
 	// end:source /src/business/Deferred.js
 	// source /src/business/EventEmitter.js
 	var EventEmitter = (function(){
@@ -9750,6 +9748,8 @@ function __eval(source, include) {
 					 */
 					setDOMLibrary: function(lib) {
 						domLib = lib;
+						
+						domLib_initialize();
 					},
 			
 			
@@ -10129,7 +10129,7 @@ function __eval(source, include) {
 			 *	Bind Closest Controller Handler Function to dom event(s)
 			 */
 		
-			mask.registerAttrHandler('x-signal', 'client', function(node, attrValue, model, cntx, element, controller) {
+			mask.registerAttrHandler('x-signal', 'client', function(node, attrValue, model, ctx, element, controller) {
 		
 				var arr = attrValue.split(';'),
 					signals = '',
@@ -10402,7 +10402,10 @@ function __eval(source, include) {
 		// end:source ../src/compo/signals.js
 	
 		// source ../src/jcompo/jCompo.js
-		(function(){
+		// try to initialize the dom lib, or is then called from setDOMLibrary
+		domLib_initialize();
+		
+		function domLib_initialize(){
 		
 			if (domLib == null || domLib.fn == null)
 				return;
@@ -10533,7 +10536,7 @@ function __eval(source, include) {
 				
 			}());
 		
-		}());
+		};
 		
 		// end:source ../src/jcompo/jCompo.js
 	
--- index-dev.js
+++ index-dev.js
@@ -3,7 +3,7 @@
 	
 	// source /src/license.txt
 /*!
- * ClassJS v1.0.44
+ * ClassJS v1.0.45
  * Part of the Atma.js Project
  * http://atmajs.com/
  *
@@ -21,18 +21,11 @@
 			? global
 			: window
 			,
-		_isCommonJS = false
 		_exports
 		;
 
     
-	if (typeof exports !== 'undefined' && (root == null || root === exports || root === _global)){
-		// raw commonjs module
-		_isCommonJS = true;
-        root = exports;
-    }
-	
-    _exports = root || _global;
+	_exports = root || _global;
     
 
     function construct(){
@@ -48,9 +41,8 @@
 	// Browser OR Node
     construct();
 	
-	if (_isCommonJS) 
+	if (typeof module !== 'undefined') 
 		module.exports = _exports.Class;
-	
 	
 }(this, function(global, exports){
 	"use strict";
@@ -1355,120 +1347,126 @@
 	// source /src/business/Deferred.js
 	function Deferred(){}
 	
-	Deferred.prototype = {
-		_isAsync: true,
-			
-		_done: null,
-		_fail: null,
-		_always: null,
-		_resolved: null,
-		_rejected: null,
+	(function(){
 		
-		defer: function(){
-			this._rejected = null;
-			this._resolved = null;
-		},
-		
-		resolve: function() {
-			this._fail = null;
-			this._resolved = arguments;
-	
-			var _done = this._done,
-				_always = this._always,
+		Deferred.prototype = {
+			_isAsync: true,
 				
-				imax, i;
+			_done: null,
+			_fail: null,
+			_always: null,
+			_resolved: null,
+			_rejected: null,
 			
-			this._done = null;
-			this._always = null;
+			defer: function(){
+				this._rejected = null;
+				this._resolved = null;
+			},
 			
-			if (_done != null) {
-				imax = _done.length;
-				i = -1;
-				while ( ++i < imax ) {
-					fn_apply(_done[i], this, arguments);
-				}
-				_done.length = 0;
+			resolve: function() {
+				var done = this._done,
+					always = this._always
+					;
+				
+				this._resolved = arguments;
+				
+				dfr_clearListeners(this);
+				arr_callOnce(done, this, arguments);
+				arr_callOnce(always, this, [ this ]);
+				
+				return this;
+			},
+			
+			reject: function() {
+				var fail = this._fail,
+					always = this._always
+					;
+				
+				this._rejected = arguments;
+				
+				dfr_clearListeners(this);
+				arr_callOnce(fail, this, arguments);
+				arr_callOnce(always, this, [ this ]);
+		
+				return this;
+			},
+			
+			resolveDelegate: function(){
+				return fn_proxy(this.resolve, this);
+			},
+			
+			rejectDelegate: function(){
+				return fn_proxy(this.reject, this);
+			},
+			
+			done: function(callback) {
+				
+				return dfr_bind(
+					this,
+					this._resolved,
+					this._done || (this._done = []),
+					callback
+				);
+			},
+			
+			fail: function(callback) {
+				
+				return dfr_bind(
+					this,
+					this._rejected,
+					this._fail || (this._fail = []),
+					callback
+				);
+			},
+			
+			always: function(callback) {
+				
+				return dfr_bind(
+					this,
+					this._rejected || this._resolved,
+					this._always || (this._always = []),
+					callback
+				);
+			},
+		};
+	
+		// PRIVATE
+		
+		function dfr_bind(dfr, arguments_, listeners, callback){
+			if (callback == null) 
+				return dfr;
+			
+			if ( arguments_ != null) 
+				fn_apply(callback, dfr, arguments_);
+			else 
+				listeners.push(callback);
+			
+			return dfr;
+		}
+		
+		function dfr_clearListeners(dfr) {
+			dfr._done = null;
+			dfr._fail = null;
+			dfr._always = null;
+		}
+		
+		function arr_callOnce(arr, ctx, args) {
+			if (arr == null) 
+				return;
+			
+			var imax = arr.length,
+				i = -1,
+				fn;
+			while ( ++i < imax ) {
+				fn = arr[i];
+				
+				if (fn) 
+					fn_apply(fn, ctx, args);
 			}
-	
-			if (_always != null) {
-				imax = _always.length;
-				i = -1;
-				while ( ++i < imax ) {
-					_always[i].call(this, this);
-				}
-			}
-	
-			return this;
-		},
+			arr.length = 0;
+		}
 		
-		reject: function() {
-			this._done = null;
-			this._rejected = arguments;
-			
-			var _fail = this._fail,
-				_always = this._always,
-				imax, i;
-			
-			this._fail = null;
-			this._always = null;
-	
-			if (_fail != null) {
-				imax = _fail.length;
-				i = -1;
-				while ( ++i < imax ) {
-					fn_apply(_fail[i], this, arguments);
-				}
-			}
-	
-			if (_always != null) {
-				imax = _always.length;
-				i = -1;
-				while ( ++i < imax ) {
-					_always[i].call(this, this);
-				}
-			}
-	
-			return this;
-		},
-		
-		resolveDelegate: function(){
-			return fn_proxy(this.resolve, this);
-		},
-		
-		rejectDelegate: function(){
-			return fn_proxy(this.reject, this);
-		},
-		
-		done: function(callback) {
-			if (this._resolved != null)
-				fn_apply(callback, this, this._resolved);
-			else
-				(this._done || (this._done = [])).push(callback);
-	
-			return this;
-		},
-		
-		fail: function(callback) {
-			
-			if (this._rejected != null)
-				fn_apply(callback, this, this._rejected);
-			else
-				(this._fail || (this._fail = [])).push(callback);
-	
-			return this;
-		},
-		
-		always: function(callback) {
-		
-			if (this._rejected != null || this._resolved != null)
-				callback.call(this, this);
-			else
-				(this._always || (this._always = [])).push(callback);
-	
-			return this;
-		},
-	};
-	
+	}());
 	// end:source /src/business/Deferred.js
 	// source /src/business/EventEmitter.js
 	var EventEmitter = (function(){
@@ -2548,17 +2546,17 @@
 	                if (this._ensureFree() === false) 
 	                    return this;
 	                
-	                if (this._id) {
-	                    db_patchSingle(
-	                        this._coll,
-	                        this._id, patch,
-	                        fn_proxy(this._completed, this)
-	                    );
-	                }
-	                else {
-	                    this._completed('<class:patch> `_id` is not defined');
-	                }
+	                if (this._id == null)
+	                    return this._completed('<class:patch> `_id` is not defined');
 	                
+	                obj_patch(this, patch);
+	                db_patchSingle(
+	                    this._coll,
+	                    this._id,
+	                    patch,
+	                    fn_proxy(this._completed, this)
+	                );
+	            
 	                return this;
 	            },
 	            
@@ -2598,23 +2596,22 @@
 	            
 	            _ensureFree: function(){
 	                if (this._busy) {
-	                    console.warn('<mongo:single> requested transport, but is busy by the same instance.');
+	                    console.warn('<mongo:single> transport requested, but is busy for the same instance.');
 	                    return false;
 	                }
 	                
+	                this.defer();
 	                this._busy = true;
-	                this._resolved = null;
-	                this._rejected = null;
 	                
 	                return true;
 	            },
 	            _completed: function(error){
 	                this._busy = false;
 	                
-	                if (error) 
-	                    this.reject(error);
-	                else
-	                    this.resolve(this);
+	                return error 
+	                    ? this.reject(error)
+	                    : this.resolve(this)
+	                    ;
 	            },
 	            _fetched: function(error, json) {
 	                this.deserialize(json);
@@ -2629,9 +2626,7 @@
 	                    if (is_Array(array) && array.length === 1) 
 	                        this._id = array[0]._id
 	                    else 
-	                        console.error('<mongo:insert-single> expection an array in callback');
-	                    
-	                    
+	                        console.error('<mongo:insert-single> expected an array in callback');
 	                }
 	                
 	                this._completed(error);
